7ca34c67fe66771cbc2a622d1d60ff0d0114f713
may-ag <mayag@gmail.com>
2023-12-11 15:04:26 +0530

    add delete user functionality

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,7 +3,7 @@ import { useRef, useState , useEffect} from 'react';
 import { getDownloadURL, getStorage, ref, uploadBytes, uploadBytesResumable} from 'firebase/storage';
 
 import { app } from "../firebase";
-import { updateUserStart , updateUserSuccess ,updateUserFailure } from '../redux/user/userSlice';
+import { updateUserStart , updateUserSuccess ,updateUserFailure, deleteUserStart, deleteUserFailure, deleteUserSuccess } from '../redux/user/userSlice';
 import { useDispatch } from "react-redux";
 
 export default function Profile(){
@@ -83,6 +83,24 @@ export default function Profile(){
         dispatch(updateUserFailure(error.message));
     }
   }  
+
+  const handleDeleteUser= async()=>{
+    try{
+      dispatch(deleteUserStart());
+      const res= await fetch(`/api/user/delete/${currentUser._id}`,{
+        method:'DELETE',
+      });
+      const data = await res.json();
+      if(data.success === false){
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data))
+    }catch(error){
+      dispatch(deleteUserFailure(error.message))
+    }
+  }
+
   return(
     <div className="p-3 max-w-lg mx-auto ">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -110,7 +128,7 @@ export default function Profile(){
 
       </form>
       <div className="flex justify-between mt-5">
-      <span className="text-red-700 cursor-pointer">Delete Account</span>
+      <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete Account</span>
       <span className="text-red-700 cursor-pointer">SignOut</span>
       </div>
 
