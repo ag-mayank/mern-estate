1771634c5f89e955733d72e9a142f596a233df06
may-ag <mayag@gmail.com>
2023-12-12 15:42:36 +0530

    Complete Listitng Page UI

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -5,6 +5,7 @@ import { getDownloadURL, getStorage, ref, uploadBytes, uploadBytesResumable} fro
 import { app } from "../firebase";
 import { updateUserStart , updateUserSuccess ,updateUserFailure, deleteUserStart, deleteUserFailure, deleteUserSuccess, signOutUserStart } from '../redux/user/userSlice';
 import { useDispatch } from "react-redux";
+import { Link } from 'react-router-dom'; 
 
 export default function Profile(){
   const fileRef = useRef(null);
@@ -141,6 +142,9 @@ export default function Profile(){
         <input type="email" placeholder="email" defaultValue={currentUser.username}  id="email" className="border p-3  rounded-lg" onChange={handleChange} />
         <input type="password" placeholder="password"  id="password" className="border p-3  rounded-lg"/>
         <button disabled={loading} className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">{loading ? 'Loading...' :'Update'}</button>
+
+        <Link className=" bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95" to={"/create-listing"}>Create Listing</Link> 
+
 
 
       </form>
