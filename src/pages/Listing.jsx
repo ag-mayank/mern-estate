44694c7e10b92376c98b4eb44c3a453dd8da0286
may-ag <mayag@gmail.com>
2023-12-15 16:33:44 +0530

    Add contact landlord functionality in the listing page

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,72 +1,78 @@
 
-
-import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom";
-import {Swiper , SwiperSlide} from 'swiper/react';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore from 'swiper';
-import { Navigation } from "swiper/modules";
-import 'swiper/css/bundle';
 import { useSelector } from 'react-redux';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css/bundle';
 import {
-    FaBath,
-    FaBed,
-    FaChair,
-    FaMapMarkedAlt,
-    FaMapMarkerAlt,
-    FaParking,
-    FaShare,
-  } from 'react-icons/fa';
+  FaBath,
+  FaBed,
+  FaChair,
+  FaMapMarkedAlt,
+  FaMapMarkerAlt,
+  FaParking,
+  FaShare,
+} from 'react-icons/fa';
+import Contact from '../components/Contact';
 
+// https://sabe.io/blog/javascript-format-numbers-commas#:~:text=The%20best%20way%20to%20format,format%20the%20number%20with%20commas.
 
 export default function Listing() {
-    SwiperCore.use([Navigation]);
-    const [listing , setListing]=useState(null)
-    const [loading , setLoading]=useState(false);
-    const [error,setError]=useState(false);
-    const[copied , setCopied] =useState(false);
+  SwiperCore.use([Navigation]);
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [copied, setCopied] = useState(false);
+  const [contact, setContact] = useState(false);
+  const params = useParams();
+  const { currentUser } = useSelector((state) => state.user);
 
-    const params = useParams()
-    useEffect(()=>{
-        const fetchListing = async()=>{
-           try{
-            setLoading(true)
-            const res=await fetch(`/api/listing/get/${params.listingId}`)
-            const data= await res.json();
-            if(data.success ===false){
-                setError(true)
-                setLoading(false)
-
-                return;
-           }
-           setListing(data);
-           setLoading(false);
-           setError(false)
-            }catch(error){
-                setError(true)
-                setLoading(false)
-            }
-            
+  useEffect(() => {
+    const fetchListing = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch(`/api/listing/get/${params.listingId}`);
+        const data = await res.json();
+        if (data.success === false) {
+          setError(true);
+          setLoading(false);
+          return;
         }
-        fetchListing()
-    },[params.listingId]);
+        setListing(data);
+        setLoading(false);
+        setError(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+    fetchListing();
+  }, [params.listingId]);
 
   return (
-   <main>
-    {loading && <p className="text-center my-7 text-2xl">Loading...</p>}
-    {error && <p className="text-center my-7 text-2xl">Something Went Wrong</p>}
-    {listing && !loading && !error &&  (<div>
-        <Swiper navigation>
-            {listing.imageUrls.map((url)=>(<SwiperSlide key={url}>
-
-                <div className='h-[550px]'
-                style={{background:`url(${url}) center no-repeat`, backgroundSize:'cover'}}></div>
-                
-         </SwiperSlide>    
-           )
-         )}
-        </Swiper>
-        
-        <div className='fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer'>
+    <main>
+      {loading && <p className='text-center my-7 text-2xl'>Loading...</p>}
+      {error && (
+        <p className='text-center my-7 text-2xl'>Something went wrong!</p>
+      )}
+      {listing && !loading && !error && (
+        <div>
+          <Swiper navigation>
+            {listing.imageUrls.map((url) => (
+              <SwiperSlide key={url}>
+                <div
+                  className='h-[550px]'
+                  style={{
+                    background: `url(${url}) center no-repeat`,
+                    backgroundSize: 'cover',
+                  }}
+                ></div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+          <div className='fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer'>
             <FaShare
               className='text-slate-500'
               onClick={() => {
@@ -85,11 +91,11 @@ export default function Listing() {
           )}
           <div className='flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4'>
             <p className='text-2xl font-semibold'>
-              {listing.name} - ${' ' }
+              {listing.name} - ${' '}
               {listing.offer
                 ? listing.discountPrice.toLocaleString('en-US')
                 : listing.regularPrice.toLocaleString('en-US')}
-              {listing.type === 'rent' && '/month'}
+              {listing.type === 'rent' && ' / month'}
             </p>
             <p className='flex items-center mt-6 gap-2 text-slate-600  text-sm'>
               <FaMapMarkerAlt className='text-green-700' />
@@ -131,15 +137,19 @@ export default function Listing() {
                 {listing.furnished ? 'Furnished' : 'Unfurnished'}
               </li>
             </ul>
-
-            
-
-             
-              
-              </div>
-         </div>
-    )}
-   </main>
+            {currentUser && listing.userRef !== currentUser._id && !contact && (
+              <button
+                onClick={() => setContact(true)}
+                className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'
+              >
+                Contact landlord
+              </button>
+            )}
+            {contact && <Contact listing={listing} />}
+          </div>
+        </div>
+      )}
+    </main>
   );
 }
 
